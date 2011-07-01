a5e94d0eb5e9ec28777bdd1615257c6ffa7a14e9
Gray Olson <friends.gray@gmail.com>
2011-06-30 19:20:55 -0700

    added files

---
+++ Wrapper/CC3PhysicsObject3D.h
@@ -0,0 +1,78 @@
+/*
+ * CC3: http://isgl3d.com
+ *
+ * Copyright (c) 2010-2011 Stuart Caunt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+#import <Foundation/Foundation.h>
+
+@class CC3Node;
+@class CC3Vector3D;
+class btRigidBody;
+
+/**
+ * The CC3PhysicsObject3D contains both an CC3Node and a btRigidBody providing a strong link between both
+ * the physicaly object and its rendered peer.
+ * 
+ * Note that when the CC3PhysicsObject3D is deleted, the associated btRigidBody, the btCollisionShape and
+ * the btMotionState are also deleted. During construction, The CC3Node is retained and when the
+ * CC3PhysicsObject3D is deleted the CC3Node is released.
+ */
+@interface CC3PhysicsObject3D : NSObject {
+	
+@private
+	CC3Node * _node;
+	btRigidBody * _rigidBody;
+
+}
+
+/**
+ * Returns the associated CC3Node.
+ */
+@property (readonly) CC3Node * node;
+
+/**
+ * Returns the associated btRigidBody.
+ */
+@property (readonly) btRigidBody * rigidBody;
+
+/**
+ * Initialises the CC3PhysicsObject3D with an CC3Node and a btRigidBody.
+ * @param node The CC3Node.
+ * @param rigidBody The btRigidBody.
+ */
+- (id) initWithNode:(CC3Node *)node andRigidBody:(btRigidBody *)rigidBody;
+
+/**
+ * Applies a force, defined as a vector, to the btRigidBody at a given vector position.
+ * @param force The force to be applied.
+ * @param position The position at which the force is applied.
+ */
+- (void) applyForce:(CC3Vector)force withPosition:(CC3Vector)position;
+
+- (void) applyImpulse:(CC3Vector)impulse withPosition:(CC3Vector)position;
+
+- (CC3Vector) getGlobalPosition;
+
+- (void) setGlobalPosition:(CC3Vector)position;
+
+@end
---
+++ Wrapper/CC3PhysicsWorld.h
@@ -0,0 +1,96 @@
+/*
+ * CC3: http://isgl3d.com
+ *
+ * Copyright (c) 2010-2011 Stuart Caunt
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
+#import "CC3Node.h"
+
+@class CC3PhysicsObject3D;
+class btRigidBody;
+class btDiscreteDynamicsWorld;
+class btCollisionShape;
+
+/**
+ * The CC3PhysicsWorld provides a wrapper to the btDiscreteDynamicsWorld and contains all the CC3PhysicsObject3D objects. 
+ * It inherits from CC3Node so is added directly to the scene. At every frame it updates automatically the 
+ * btDiscreteDynamicsWorld (the physics simulation is updated) and hence the transformations of the physics
+ * objects (btRigidBody) are updated.
+ */
+@interface CC3PhysicsWorld : CC3Node {
+	
+@private
+	btDiscreteDynamicsWorld * _discreteDynamicsWorld;
+
+	NSDate * _lastStepTime;
+	NSMutableArray * _physicsObjects;
+
+}
+
+/**
+ * Initialises the CC3PhysicsWorld;
+ */
+- (id) init;
+
+/**
+ * Sets the btDiscreteDynamicsWorld in which the Bullet Physics simulation takes place. The btDiscreteDynamicsWorld is
+ * automatically stepped at every rendered frame.
+ */
+- (void) setDiscreteDynamicsWorld:(btDiscreteDynamicsWorld *)discreteDynamicsWorld;
+
+/**
+ * Adds a new CC3PhysicsObject3D containing both a btRigidBody and an CC3Node. Note, this DOES NOT add the
+ * node contained in the physics object to the scene: this needs to be done independently. The btRigidBody is
+ * added to the btDiscreteDynamicsWorld.
+ * @param physicsObject The CC3PhysicsObject3D containing both btRigidBody and CC3Node.
+ */
+- (void) addPhysicsObject:(CC3PhysicsObject3D *)physicsObject;
+
+/**
+ * Removes an CC3PhysicsObject3D from the physics world. Note, this DOES remove the associated CC3Node
+ * from its parent node in the scene. The btRigidBody is removed from the btDiscreteDynamicsWorld.
+ * @param physicsObject The CC3PhysicsObject3D to remove.
+ */
+- (void) removePhysicsObject:(CC3PhysicsObject3D *)physicsObject;
+
+/**
+ * Sets the size and direction of gravity in the physics simulation.
+ * @param x The x component of the gravity vectory.
+ * @param y The y component of the gravity vectory.
+ * @param z The z component of the gravity vectory.
+ */
+- (void) setGravity:(float)x y:(float)y z:(float)z;
+
+/**
+ * Utility method to create an CC3PhysicsObject3D from an CC3Node and a btCollisionShape. A btRigidBody is created for
+ * the shape with an CC3MotionShape containing the node. Both btRigidBody and CC3Node are returned in the CC3PhysicsObject3D.
+ * @param node The CC3Node to be manipulated as a result of the physics simulation.
+ * @param shape The shape of the object.
+ * @param mass The mass of the object.
+ * @param restitution The restitution of the object.
+ * @return (autorelease) The created CC3PhysicsObject3D containing the btRigidBody and the CC3Node.
+ */
+- (CC3PhysicsObject3D *) createPhysicsObject:(CC3Node *)node shape:(btCollisionShape *)shape mass:(float)mass restitution:(float)restitution;
+
+- (void) udpateGlobalTransformation:(CC3GLMatrix *)parentTransformation;
+
+@end
